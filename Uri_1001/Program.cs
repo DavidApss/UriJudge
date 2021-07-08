9e9568de0f29014c78dfc26a0a427586fd11b2bd
David Santos <david.adriel20172@gmail.com>
2021-07-08 10:20:38 -0300

    Uri_1001

---
+++ Uri_1001/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Uri_1001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            int x = a + b;
+
+            Console.WriteLine($"X = {x}");
+        }
+    }
+}
