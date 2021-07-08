a76db7064306d8cf6bc5a1a4fa67afec4ce3591f
David Santos <david.adriel20172@gmail.com>
2021-07-08 10:40:53 -0300

    Uri_1002

---
+++ Uri_1002/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+using System;
+
+namespace Uri_1002
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n = 3.14159, raio, area;
+
+            raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            area = n * Math.Pow(raio, 2);
+
+            Console.WriteLine($"A={area.ToString("F4", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
