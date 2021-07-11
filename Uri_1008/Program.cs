0b48d1d8e0c5c8491651d9fa6f4e66121cf654dc
David Santos <david.adriel20172@gmail.com>
2021-07-11 16:03:07 -0300

    Uri_1008

---
+++ Uri_1008/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+using System;
+
+namespace Uri_1008
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int codigoFuncionario = int.Parse(Console.ReadLine());
+            int horasTrabalhadas = int.Parse(Console.ReadLine());
+            double valorPorHora = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double salario = horasTrabalhadas * valorPorHora;
+
+            Console.WriteLine($"NUMBER = {codigoFuncionario}");
+            Console.WriteLine($"SALARY = U$ {salario.ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
