4e5b342a9159cdbe3e74d592b3116585b896c23d
David Santos <david.adriel20172@gmail.com>
2021-07-11 15:45:15 -0300

    Uri_1006

---
+++ Uri_1006/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+using System;
+
+namespace Uri_1006
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double b = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double c = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double media = ((a * 2) + (b * 3) + (c * 5)) / 10;
+
+            Console.WriteLine($"MEDIA = {media.ToString("F1", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
