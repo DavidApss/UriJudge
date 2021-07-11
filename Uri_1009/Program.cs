f1e8341e82ae257645f3ee9d711d6654036df775
David Santos <david.adriel20172@gmail.com>
2021-07-11 16:11:49 -0300

    Uri_1009

---
+++ Uri_1009/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+using System;
+
+namespace Uri_1009
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome = Console.ReadLine();
+            double salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double totalVendas = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double totalReceber = (totalVendas * 15 / 100) + salario;
+
+            Console.WriteLine($"TOTAL = R$ {totalReceber.ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
