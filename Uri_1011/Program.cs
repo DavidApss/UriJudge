a9fd4c488e86e1365bac39739a26b0fb8c1e81e2
David Santos <david.adriel20172@gmail.com>
2021-07-14 09:50:08 -0300

    Uri_1011

---
+++ Uri_1011/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+using System;
+
+namespace Uri_1011
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double r = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double volume = (4.0 / 3 * 3.14159) * Math.Pow(r, 3);
+
+            Console.WriteLine($"VOLUME = {volume.ToString("F3", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
