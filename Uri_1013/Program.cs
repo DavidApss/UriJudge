a7bf48fba0926b7a0e8f408bb20e51e87279edf7
David Santos <david.adriel20172@gmail.com>
2021-08-02 21:54:31 -0300

    Uri_1013

---
+++ Uri_1013/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Uri_1013
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] vs = Console.ReadLine().Split(' ');
+
+            int a = int.Parse(vs[0]);
+            int b = int.Parse(vs[1]);
+            int c = int.Parse(vs[2]);
+
+            int maior = ((a + b) + Math.Abs(a - b)) / 2;
+            maior = ((maior + c) + Math.Abs(maior - c)) / 2;
+
+            Console.WriteLine($"{maior} eh o maior");
+        }
+    }
+}
