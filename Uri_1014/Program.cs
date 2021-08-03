c7e6a2774d7c2e52bd1053f7c744485305894bb0
David Santos <david.adriel20172@gmail.com>
2021-08-02 22:06:57 -0300

    Uri_1014

---
+++ Uri_1014/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+namespace Uri_1014
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            double y = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double consumoMedio = x / y;
+
+            Console.WriteLine($"{consumoMedio.ToString("F3", CultureInfo.InvariantCulture)} km/l");
+        }
+    }
+}
